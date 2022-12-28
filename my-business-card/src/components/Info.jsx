0804638ced5e63711a4812b78c4ce10823207776
ling zhang <lingzhangmyself@gmail.com>
2022-12-29 11:52:16 +1300

    rendered myImage

---
+++ my-business-card/src/components/Info.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+import myImage from "../images/my-image.jpeg"
+
+export default function Info() {
+  return (
+    <>
+      <img src={myImage} className="myImage"/>
+     
+    </>
+  )
+} 
